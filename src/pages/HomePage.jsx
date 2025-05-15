5adbf515eba94f11174635e4aa71cf4ac9ac6b17
airidziox <airidas.augulis@gmail.com>
2025-05-15 11:22:02 +0300

    categories created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 import http from "../plugins/https.jsx";
 import EventCard from "../components/EventCard.jsx";
 import HeroCarousel from "../components/HeroCarousel.jsx";
+import Categories from "../components/Categories.jsx";
 
 const HomePage = () => {
 
@@ -25,10 +26,15 @@ const HomePage = () => {
     }, [events]);
 
     return (
-        <div>
+        <div className="d-flex flex-column align-items-center">
             <div>
                 <HeroCarousel/>
             </div>
+
+            <div className="mt-5 py-7">
+                <Categories/>
+            </div>
+            
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-5">
                 {events?.map(event =>
                     <EventCard key={event._id} event={event}/>
